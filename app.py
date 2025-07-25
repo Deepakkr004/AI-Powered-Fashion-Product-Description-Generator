f40a4817e09c505d245b1bf9c7e9f5097259854b
Deepakkr004 <157491438+Deepakkr004@users.noreply.github.com>
2025-07-25 08:55:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def index():
     return render_template("index.html", description=description, filename=filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
